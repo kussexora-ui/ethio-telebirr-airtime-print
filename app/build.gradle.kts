22ccf3efc1e474a0a012ee7bc5bb3283b037ea27
kussexora-ui <kussexora@gmail.com>
2026-08-06 18:28:20 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,3 +16,13 @@ signingConfigs {
       }
     }
   }
+
+  buildTypes {
+    release {
+      isCrunchPngs = false
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      signingConfig = signingConfigs.getByName("release")
+    }
+    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+  }
